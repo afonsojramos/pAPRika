004e0b3203aa5c424e45b0f9ae6f47bf93e3f0fd
Diogo Campos <d.diogocampos@gmail.com>
2019-06-05 00:08:41 +0100

    Transition from hardcoded visit to generic visit function

--- src/code-interface.ts
+++ src/code-interface.ts
@@ -4,58 +4,54 @@ import { runTestSuiteOnce } from "./tests-interface";
 import Replacement from "./replacement";
 import SuggestionActionProvider from "./suggestion-action-provider";
 import * as vscode from 'vscode';
-    
-const mathOperators: number[] = [ts.SyntaxKind.SlashToken, ts.SyntaxKind.AsteriskToken, ts.SyntaxKind.MinusToken, ts.SyntaxKind.PlusToken];
 
-class Variation {
-    constructor(readonly variation: string, readonly replacementList: Replacement[]) {}
+interface SyntaxKindToTextMap {
+    [key: number]: string;
 }
-  
+
+const mathOperators: number[] = [ts.SyntaxKind.SlashToken, ts.SyntaxKind.AsteriskToken, ts.SyntaxKind.MinusToken, ts.SyntaxKind.PlusToken];
+const mathOperatorsToText: SyntaxKindToTextMap = {
+    [ts.SyntaxKind.SlashToken]: "/",
+    [ts.SyntaxKind.AsteriskToken]: "*",
+    [ts.SyntaxKind.MinusToken]: "-",
+    [ts.SyntaxKind.PlusToken]: "+"
+};
+
 function generateVariations(filePath: string, functionName: string, document: vscode.TextDocument, suggestionActionProvider: SuggestionActionProvider) {
-    let codeVariations: Variation[] = [];
-    
     let functionNode: ts.FunctionDeclaration | undefined = getFunctionDeclaration(filePath, functionName);
-    
-    // TODO hardcoded
-    if (functionNode !== undefined) {
-        let block: ts.Block | undefined = functionNode!.getChildren().find(ts.isBlock);
-        let syntaxList: ts.Node | undefined = block!.getChildren().find((child) => child.kind === ts.SyntaxKind.SyntaxList);
-        let returnStatement: ts.ReturnStatement | undefined = syntaxList!.getChildren().find(ts.isReturnStatement);
-        let binaryExpression: ts.BinaryExpression | undefined = returnStatement!.getChildren().find(ts.isBinaryExpression);
-        let mathOperator: ts.Node | undefined = binaryExpression!.getChildren().find(isMathOperator);
+    let replacementList: Replacement[] = new Array();
+    visitDoReplacements(functionNode!, replacementList);
 
-        mathOperators.filter((op) => op !== mathOperator!.kind).forEach((operator) => {
-            let replacementList: Replacement[] = new Array();
-            visitReplaceOperator(functionNode!, operator, replacementList);
-            codeVariations.push(new Variation(replaceLines(readFileSync(filePath).toString(), replacementList), replacementList));
-        });
-    }
-
-    for (let index = 0; index < codeVariations.length; index++) {
-        let variation: string = codeVariations[index].variation;
+    for (let index = 0; index < replacementList.length; index++) {
+        let variation: string = replaceLines(readFileSync(filePath).toString(), replacementList[index]);
         let variationFileName: string = `/home/diogocampos/workspace/feup/diss/project/sample-project/test/tmp${functionName}${index}.ts`;
         writeFileSync(variationFileName, variation);
-        runTestSuiteOnce(filePath, variationFileName, document, codeVariations[index].replacementList, suggestionActionProvider);
+        runTestSuiteOnce(filePath, variationFileName, document, [replacementList[index]], suggestionActionProvider);
     }
 }
 
-function visitReplaceOperator(node: ts.Node, operator: number, replacementList: Replacement[]) {
+function visitDoReplacements(node: ts.Node, replacementList: Replacement[]) {
     if (ts.isBinaryExpression(node)) {
-        const newNode: ts.Node = ts.createBinary(<ts.Expression> node.getChildAt(0), operator, <ts.Expression> node.getChildAt(2));
-        const replacement: Replacement = Replacement.replace(node.getStart(), node.getEnd(), getTSNodeText(node), getTSNodeText(newNode));
-        replacementList.push(replacement);
+        generateMathOperatorVariants(node, replacementList);
     }
 
-    ts.forEachChild(node, (child) => visitReplaceOperator(child, operator, replacementList));
+    ts.forEachChild(node, (child) => visitDoReplacements(child, replacementList));
 }
 
-function replaceLines(originalFile: string, replacementList: Replacement[]): string {
+function generateMathOperatorVariants(node: ts.Node, replacementList: Replacement[]) {
+    const operator: ts.Node = node.getChildAt(1);
+    if (mathOperators.includes(operator.kind)) {
+        mathOperators.filter((op) => op !== operator.kind).forEach((newOperator) => {
+            const operatorText = mathOperatorsToText[newOperator];
+            const replacement: Replacement = Replacement.replace(operator.getStart(), operator.getEnd(), getTSNodeText(operator), operatorText);
+            replacementList.push(replacement);
+        });
+    }
+}
+
+function replaceLines(originalFile: string, replacement: Replacement): string {
     let newFile: string = originalFile;
-
-    replacementList.forEach((replacement: Replacement) => {
-        newFile = newFile.slice(0, replacement.start) + replacement.newText + newFile.slice(replacement.end, newFile.length);
-    });
-
+    newFile = newFile.slice(0, replacement.start) + replacement.newText + newFile.slice(replacement.end, newFile.length);
     return newFile;
 }
 
@@ -100,24 +96,6 @@ function isFunctionName(comparable: string, functionNode: ts.FunctionDeclaration
 
 function syntaxKindToName(kind: ts.SyntaxKind): string {
     return (<any>ts).SyntaxKind[kind];
-}
-
-function isMathOperator(node: ts.Node) {
-    return mathOperators.includes(node.kind);
-}
-
-function getExpressionStatementList(node: ts.Node): ts.Node[] {
-    let expressionList: ts.Node[] = [];
-
-    if (ts.isExpressionStatement(node)) {
-        return [node];
-    }
-
-    for (let child of node.getChildren()) {
-        expressionList = expressionList.concat(getExpressionStatementList(child));
-    }
-
-    return expressionList;
 }
 
 export {
