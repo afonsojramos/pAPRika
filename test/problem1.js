d06fec382f11b9cd5641407db2037a3b2a875792
Afonso Ramos <afonsojorgeramos@gmail.com>
2020-03-22 17:17:07 +0000

    🐛 fixed problem1 bug

--- test/problem1.js
+++ test/problem1.js
@@ -1,4 +1,14 @@
-import { equal } from 'assert';
+let assert = require('assert');
+
+/*
+ 
+Problem 1: Given a string, return a subtring within the given limits.
+ 
+Example:
+
+A substring of "This is a string" is "is"
+ 
+*/
 
 function mySubstring(str, i1, i2) {
     return str.substring(i1, i2);
@@ -6,10 +16,10 @@ function mySubstring(str, i1, i2) {
 
 describe('mySubstring', function() {
     it('#fix {mySubstring}', function() {
-        equal(mySubstring('This is a string', 1, 2), 'hi');
+        assert.equal(mySubstring('This is a string', 1, 2), 'hi');
     });
 
     it('#fix {mySubstring}', function() {
-        equal(mySubstring('This is a string', 6, 8), 's a');
+        assert.equal(mySubstring('This is a string', 6, 8), 's a');
     });
 });
