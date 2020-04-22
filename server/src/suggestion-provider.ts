af954bf6c35042aa97c9dec404cdd8a011618194
Afonso Jorge Ramos <afonsojorgeramos@gmail.com>
2020-04-22 20:17:25 +0100

    suggestion-provider overhaul

--- server/src/suggestion-provider.ts
+++ server/src/suggestion-provider.ts
@@ -1,64 +1,50 @@
-import * as vscode from 'vscode';
+import { Diagnostic, DiagnosticSeverity, TextDocument, Range, CodeActionContext, CodeAction, CancellationToken, Command } from 'vscode-languageserver';
 import Replacement from './replacement';
 
-export default class SuggestionActionProvider implements vscode.CodeActionProvider {
-    private static commandId: string = 'extension.runCodeAction';
-    private diagnosticCollection: vscode.DiagnosticCollection;
-    private command: vscode.Disposable;
+function suggestChangesLint(textDocument: TextDocument, replacementList: Replacement[]) {
+	let diagnostics: Diagnostic[] = new Array<Diagnostic>();
 
-    constructor(subscriptions: vscode.Disposable[]) {
-    	this.diagnosticCollection = vscode.languages.createDiagnosticCollection();
-    	this.command = vscode.commands.registerCommand(SuggestionActionProvider.commandId, this.runCodeAction, this);
-    	subscriptions.push(this);
-    }
-    
-    public dispose(): void {
-    	this.diagnosticCollection.dispose();
-    	this.command.dispose();
-    }
-    
-    public cleanSuggestions(): void {
-    	this.diagnosticCollection.clear();
-    }
-    
-    public suggestChangesLint(textDocument: vscode.TextDocument, replacementList: Replacement[]) {
-    	let diagnostics: vscode.Diagnostic[] = new Array<vscode.Diagnostic>();
-
-    	replacementList.forEach((replacement: Replacement) => {
-    		const severity: number = vscode.DiagnosticSeverity.Error;
-    		const message: string = 'Replace: ' + replacement.oldText + ' ==> ' + replacement.newText;
-    		const range: vscode.Range = new vscode.Range(textDocument.positionAt(replacement.start), textDocument.positionAt(replacement.end));
-    		const diagnostic: vscode.Diagnostic = new vscode.Diagnostic(range, message, severity);
-    		diagnostics.push(diagnostic);
-    	});
-
-    	this.diagnosticCollection.set(textDocument.uri, diagnostics);
-    }
-
-    public provideCodeActions(document: vscode.TextDocument, range: vscode.Range | vscode.Selection, context: vscode.CodeActionContext, token: vscode.CancellationToken): vscode.ProviderResult<(vscode.Command | vscode.CodeAction)[]> {
-    	return context.diagnostics.map((diagnostic) => {
-    		return {
-    			title: 'Accept code suggestion',
-    			command: SuggestionActionProvider.commandId,
-    			arguments: [document, diagnostic.range, diagnostic.message]
-    		};
-    	});
-    }
-    
-    private runCodeAction(document: vscode.TextDocument, range: vscode.Range, message: string): any {
-    	let fromRegex: RegExp = /.*Replace:(.*)==>.*/g;
-    	let fromMatch: RegExpExecArray | null = fromRegex.exec(message.replace(/\s/g, ''));
-    	let from = fromMatch![1];
-    	let to:string = document.getText(range).replace(/\s/g, '');
-    	if (from === to) {
-    		let newText = /[\s\S]*==>\s([\s\S]*)/g.exec(message)![1];
-    		let edit = new vscode.WorkspaceEdit();
-    		edit.replace(document.uri, range, newText);
-    		this.diagnosticCollection.clear();
-    		return vscode.workspace.applyEdit(edit);
-    	} else {
-    		vscode.window.showErrorMessage('The suggestion was not applied because the source code changed.');
-    		this.diagnosticCollection.clear();
-    	}
-    }
+	replacementList.forEach((replacement: Replacement) => {
+		let diagnostic: Diagnostic = {
+			severity: DiagnosticSeverity.Error,
+			range: {
+				start: textDocument.positionAt(replacement.start),
+				end: textDocument.positionAt(replacement.end)
+			},
+			message: 'Replace: ' + replacement.oldText + ' ==> ' + replacement.newText,
+			source: 'ProperFix'
+		};
+		diagnostics.push(diagnostic);
+	});
 }
+
+function provideCodeActions(document: TextDocument, range: Range | Selection, context: CodeActionContext, token: CancellationToken): CodeAction[] {
+	console.log('Range ' + range);
+	return context.diagnostics.map((diagnostic: Diagnostic) => {
+		return {
+			title: 'Accept code suggestion',
+			//command: commandId,
+			command: Command.create('ProperFix', 'extension.runCodeAction'),
+			arguments: [document, diagnostic.range, diagnostic.message]
+		};
+	});
+}
+
+function runCodeAction(document: TextDocument, range: Range, message: string): any {
+	let fromRegex: RegExp = /.*Replace:(.*)==>.*/g;
+	let fromMatch: RegExpExecArray | null = fromRegex.exec(message.replace(/\s/g, ''));
+	let from = fromMatch![1];
+	let to: string = document.getText(range).replace(/\s/g, '');
+	if (from === to) {
+		let newText = /[\s\S]*==>\s([\s\S]*)/g.exec(message)![1];
+		/* let edit = new WorkspaceEdit();
+		edit.replace(document.uri, range, newText);
+		this.diagnosticCollection.clear();
+		return vscode.workspace.applyEdit(edit); */
+	} else {
+		/* vscode.window.showErrorMessage('The suggestion was not applied because the source code changed.');
+		this.diagnosticCollection.clear(); */
+	}
+}
+
+export { suggestChangesLint, provideCodeActions, runCodeAction };
