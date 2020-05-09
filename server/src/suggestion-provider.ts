d0760980ca42a4fb8ebb1a564fe580b0303ffcec
Afonso Jorge Ramos <afonsojorgeramos@gmail.com>
2020-05-09 05:18:26 +0100

    removed unused functions

--- server/src/suggestion-provider.ts
+++ server/src/suggestion-provider.ts
@@ -27,32 +27,4 @@ export default class SuggestionProvider {
 		this.connection.sendDiagnostics({ uri: textDocument.uri, diagnostics });
 	}
 
-	public provideCodeActions(document: TextDocument, range: Range | Selection, context: CodeActionContext, token: CancellationToken): CodeAction[] {
-		console.log('Range ' + range);
-		return context.diagnostics.map((diagnostic: Diagnostic) => {
-			return {
-				title: 'Accept code suggestion',
-				//command: commandId,
-				command: Command.create('pAPRika', 'extension.runCodeAction'),
-				arguments: [document, diagnostic.range, diagnostic.message]
-			};
-		});
-	}
-
-	private runCodeAction(document: TextDocument, range: Range, message: string): any {
-		let fromRegex: RegExp = /.*Replace:(.*)==>.*/g;
-		let fromMatch: RegExpExecArray | null = fromRegex.exec(message.replace(/\s/g, ''));
-		let from = fromMatch![1];
-		let to: string = document.getText(range).replace(/\s/g, '');
-		if (from === to) {
-			let newText = /[\s\S]*==>\s([\s\S]*)/g.exec(message)![1];
-			/* let edit = new WorkspaceEdit();
-			edit.replace(document.uri, range, newText);
-			this.diagnosticCollection.clear();
-			return vscode.workspace.applyEdit(edit); */
-		} else {
-			/* vscode.window.showErrorMessage('The suggestion was not applied because the source code changed.');
-			this.diagnosticCollection.clear(); */
-		}
-	}
 }
