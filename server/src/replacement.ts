2ab3b2f5314459729768db4773151849138f78ed
Afonso Jorge Ramos <afonsojorgeramos@gmail.com>
2020-05-15 20:30:12 +0100

    replacement documentation

--- server/src/replacement.ts
+++ server/src/replacement.ts
@@ -1,4 +1,15 @@
 export default class Replacement {
+	/**
+	 * Creates an instance of a Replacement.
+	 * 
+	 * @param {number} start Start position.
+	 * @param {number} end End position.
+	 * @param {string} [oldText=''] Old text to replace.
+	 * @param {string} [newText=''] New text.
+	 * @memberof Replacement
+	 */
+	constructor(readonly start: number, readonly end: number, readonly oldText: string = '', readonly newText: string = '') {}
+	
 	public static insert(pos: number, text: string) {
 		return new Replacement(pos, pos, text);
 	}
@@ -6,6 +17,4 @@ export default class Replacement {
 	public static replace(start: number, end: number, oldText: string, newText: string) {
 		return new Replacement(start, end, oldText, newText);
 	}
-  
-	constructor(readonly start: number, readonly end: number, readonly oldText = '', readonly newText = '') {}
 }  
