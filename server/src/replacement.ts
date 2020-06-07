fbec84cbf0e1cf67dcb98c6b7916d307e84e6f3e
Afonso Jorge Ramos <afonsojorgeramos@gmail.com>
2020-06-07 21:35:13 +0100

    added Replacement documentation

--- server/src/replacement.ts
+++ server/src/replacement.ts
@@ -15,10 +15,24 @@ export default class Replacement {
 		readonly newText: string = ''
 	) {}
 
+	/**
+	 * Creates a `Replacement` insertion.
+	 *
+	 * @param {number} pos Insertion position.
+	 * @param {string} text New text.
+	 */
 	public static insert(pos: number, text: string) {
 		return new Replacement(pos, pos, text)
 	}
 
+	/**
+	 * Creates a `Replacement`.
+	 *
+	 * @param {number} start Start position.
+	 * @param {number} end End position.
+	 * @param {string} oldText Old text to replace.
+	 * @param {string} newText New text.
+	 */
 	public static replace(start: number, end: number, oldText: string, newText: string) {
 		return new Replacement(start, end, oldText, newText)
 	}
