76fe3169be5efd62433468230bf0e807cc639084
Afonso Jorge Ramos <afonsojorgeramos@gmail.com>
2020-06-14 15:42:18 +0100

    updated with new tests

--- server/src/test/examples/async.ts
+++ server/src/test/examples/async.ts
@@ -1,11 +1,18 @@
-import fc from 'fast-check'
+import { assert, asyncProperty, nat as aNat, array as anArray, boolean as aBoolean } from 'fast-check'
 import { describe, it } from 'mocha'
+import { expect } from 'chai'
 
 class AsyncQueue<T> {
 	private queue = Array<T>()
 	private waitingEnqueue: AsyncSemaphore
+	s = (test: any) => {
+		console.log(test)
 
-	constructor(readonly maxSize: number) {
+		return 's'
+		return 'si'
+	}
+
+	constructor() {
 		this.waitingEnqueue = new AsyncSemaphore(0)
 	}
 
@@ -37,23 +44,56 @@ class AsyncSemaphore {
 }
 
 describe.only('the failing test', () => {
-	it('test #1 {testAsyncQueueBehavior}', async () => {
-		await fc.assert(
-			fc.asyncProperty(
-				fc.array(
-					fc.boolean().map((b) => (b ? 'E' : 'D')),
-					1000
+	it('test #1 {AsyncSemaphore.wait}', async () => {
+		await assert(
+			asyncProperty(
+				aNat(10),
+				anArray(
+					aBoolean().map((b) => (b ? 'S' : 'W')),
+					100
 				),
-				async (ns) => testAsyncQueueBehavior(ns)
+				async (size, ops) => testSemaphore(size, ops)
 			),
-			{ numRuns: 100 }
+			{ numRuns: 3 }
 		)
+	}).timeout(30000)
+
+	it('test 2 {AsyncQueue.s}', () => {
+		const q = new AsyncQueue<number>()
+		expect(q.s('This is a string')).to.equal('si')
 	})
 })
 
+async function testSemaphore(size: number, ops: Array<'S' | 'W'>) {
+	const sem = new AsyncSemaphore(size)
+	const res = Array<boolean>()
+	const promises = Array<Promise<void>>()
+
+	let signals = 0,
+		waits = 0
+
+	for (const op of ops) {
+		if (op === 'S') {
+			signals += 1
+			sem.signal()
+		} else {
+			waits += 1
+			promises.push(
+				sem.wait().then(() => {
+					res.push(true)
+				})
+			)
+		}
+	}
+
+	await Promise.all(promises.slice(0, signals + size))
+
+	return res.length === Math.min(signals + size, waits)
+}
+
 async function testAsyncQueueBehavior(ops: Array<'E' | 'D'>): Promise<boolean> {
 	const result = new Array<number>()
-	const q = new AsyncQueue<number>(100)
+	const q = new AsyncQueue<number>()
 	const promises = Array<Promise<void>>()
 
 	let enqueues = 0,
