6fe72ffd0e29cb54c4a49a9ffc3c9bd70a1f8e96
Afonso Jorge Ramos <afonsojorgeramos@gmail.com>
2020-06-22 03:25:35 +0100

    moved examples to examples/

--- examples/src/fast-check.js
+++ examples/src/fast-check.js
@@ -1,19 +1,14 @@
-const fc = require('fast-check')
+import * as fc from 'fast-check'
 
-// Code under test
 const contains = (text, pattern) => text.indexOf(pattern) > 0
 
-// Properties
 describe('properties', () => {
-	// string text always contains itself
 	it('should always contain itself {contains}', () => {
 		fc.assert(fc.property(fc.string(), (text) => contains(text, text)))
 	})
-	// string a + b + c always contains b, whatever the values of a, b and c
 	it('should always contain its substrings {contains}', () => {
 		fc.assert(
 			fc.property(fc.string(), fc.string(), fc.string(), (a, b, c) => {
-				// Alternatively: no return statement and direct usage of expect or assert
 				return contains(a + b + c, b)
 			})
 		)
